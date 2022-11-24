ce612c56b18033c0c2a34680402bb3178b1c872e
erraticless <114945849+erraticless@users.noreply.github.com>
2022-11-24 10:55:24 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import MainStack from './navigation/MainStack';
+
+function App() {
+    return (
+        <MainStack />
+    );
+}
+export default App;
---
+++ Run.bat
@@ -0,0 +1,5 @@
+@echo off
+start "BFA" "c:\Program Files\Anywhere Software\B4A\B4A.exe"
+start
+cd c:\React\TuPedido\
+code .
